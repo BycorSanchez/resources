35dbb21df475b643cb3872c9886f27e2af45a712
Bycor Sánchez <bycor1989@gmail.com>
2018-01-02 14:57:24 +0000

    Add website

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    const activeClass = 'active-link';
+    const menuVisibleClass = 'menu-visible';
+
+    function smoothScroll(target) {
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 'linear');
+    };
+
+    function toggleMenu() {
+        $('body').toggleClass(menuVisibleClass);
+    };
+
+    function updateActive(position) {
+        $('#nav a').each(function() {
+            let link = $(this);
+            let reference = $(link.attr("href"));
+
+            let top = reference.position().top - 1;
+            let bottom = top + reference.outerHeight();
+
+            if (top <= position && bottom > position) {
+                $('#nav').find('a').removeClass(activeClass);
+                link.addClass(activeClass);
+            } else {
+                link.removeClass(activeClass);
+            }
+        });
+    };
+
+    $('#menu header').on('click', 'a', function(event) {
+        event.preventDefault();
+
+        const reference = $(this).attr('href');
+        if (reference !== '#') smoothScroll(reference);
+    });
+
+    $('#menu-icon').click(toggleMenu);
+
+    $(document).on("scroll", function() {
+        const screenHeight = window.innerHeight;
+        let position = $(this).scrollTop() + (screenHeight / 3);
+        updateActive(position);
+    });
+
+    //TODO move menu scroll when scrolling down
+});
